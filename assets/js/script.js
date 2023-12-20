2a208abebf4694912b5e02bf87de549fa66bd3d9
rasyaadmk <rasyaadmaulanakhandiyas_1313621020@mhs.unj.ac.id>
2023-12-20 23:34:38 +0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+AOS.init();
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 100) {
+        nav.classList.add('shadow');
+    } else {
+        nav.classList.remove('shadow');
+    }
+});
